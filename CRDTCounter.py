360ed97482773d626ea4a364dec2fb9ddf676ff0
cinnamon-rolling <jyng.9922@gmail.com>
2020-11-10 21:27:04 +0800

    feat: add operation crdt logic

--- CRDTCounter.py
+++ CRDTCounter.py
@@ -5,80 +5,83 @@ from concurrent.futures import ThreadPoolExecutor
 from functools import partial
 from utils import get_operation, get_peer, print_peers_and_counter, get_number_of_concurrent_operations, thread_send_msg
 
-DEBUG = 0 # 1 to on debug, 0 to off debug
+DEBUG = 0  # 1 to on debug, 0 to off debug
 HOST = '0.0.0.0'
 PORT_INIT = 8000
-DELAY = 1000 # Simulate internet delay from sender to receiver in ms
+DELAY = 1000  # Simulate internet delay from sender to receiver in ms
+
 
 def debug_log(msg):
-  if DEBUG:
-    print(f"[{datetime.now()}]: {msg}")
+    if DEBUG:
+        print(f"[{datetime.now()}]: {msg}")
+
 
 if __name__ == "__main__":
-  while True:
-    n = input("Enter the number of peers in the P2P network: ")
-    try:
-      n = int(n)
-    except ValueError:
-      print("Must be integer!")
-      continue
-    if n < 2: 
-      print("Should have at least 2 peers")
-      continue
-    elif n > 50: 
-      print("Please demo using less than 50 peers for message displaying purposes!")
-      continue
-    break
-  peers = []  # To input all the peers
-  for i in range(n):
-    p = Peer(HOST, PORT_INIT+i+1)
-    peers.append(p)
-    p.start()
-  
-  # set up p2p connection
-  for i, p in enumerate(peers):
-    for idx in range(n):
-      if i != idx:
-        p.connect_with_node(HOST, PORT_INIT+idx+1)
+    while True:
 
-  if DEBUG:
-    for i in peers:
-      debug_log(i.all_nodes)
-
-  print(f"You have successfully set up {len(peers)} peers with id ranging from 1 to {n}")
-  print("Each peer has a counter value initialized to be 0")
-
-  while True:
-
-    sleep(DELAY/1000)
-    print_peers_and_counter(peers)
-
-    _n = get_number_of_concurrent_operations(peers)
-    operations = []
-    active_peers = []
-    if not _n:
-      continue
-    for _ in range(_n):
-      while True:
-        peer = get_peer(n, peers)
-        if not peer:
-          continue
+        n = input("Enter the number of peers in the P2P network: ")
+        try:
+            n = int(n)
+        except ValueError:
+            print("Must be integer!")
+            continue
+        if n < 2:
+            print("Should have at least 2 peers")
+            continue
+        elif n > 50:
+            print(
+                "Please demo using less than 50 peers for message displaying purposes!"
+            )
+            continue
         break
-      while True:
-        result = get_operation()
-        if not result:
-          continue
-        o = result[0]
-        val = result[1]
-        break
+    peers = []  # To input all the peers
+    for i in range(n):
+        p = Peer(HOST, PORT_INIT + i + 1)
+        peers.append(p)
+        p.start()
 
-      operations.append((o, val))
-      active_peers.append(peer)
+    # set up p2p connection
+    for i, p in enumerate(peers):
+        for idx in range(n):
+            if i != idx:
+                p.connect_with_node(HOST, PORT_INIT + idx + 1)
 
-    with ThreadPoolExecutor(max_workers=_n) as worker:
-      for p in active_peers:
-        worker.submit(partial(thread_send_msg, p, DELAY), operations)
-    
+    if DEBUG:
+        for i in peers:
+            debug_log(i.all_nodes)
 
+    print(
+        f"You have successfully set up {len(peers)} peers with id ranging from 1 to {n}"
+    )
+    print("Each peer has a counter value initialized to be 0")
 
-  
+    while True:
+
+        sleep(DELAY / 1000)
+
+        _n = get_number_of_concurrent_operations(peers)
+        operations = []
+        active_peers = []
+        if not _n:
+            continue
+        for _ in range(_n):
+            while True:
+                peer = get_peer(n, peers)
+                if not peer:
+                    continue
+                break
+            while True:
+                result = get_operation()
+                if not result:
+                    continue
+                o = result[0]
+                val = result[1]
+                break
+
+            operations.append((o, val))
+            active_peers.append(peer)
+
+        # spawn thread when sending to other peers
+        with ThreadPoolExecutor(max_workers=_n) as worker:
+            for p in active_peers:
+                worker.submit(partial(thread_send_msg, p, DELAY), operations)
--- utils.py
+++ utils.py
@@ -3,70 +3,100 @@ from time import sleep
 from random import randint
 import sys
 
+
 def get_operation():
-  print('''
+    print('''
   Choose an operation from below to perform:
   I1: Increment counter by 1
   D1: Decrement counter by 1
+  M1: Multiply counter by 1
+  L1: Divide counter by 1
   In: Increment counter by n, n is a custom integer
   Dn: Decrement counter by n, n is a custom integer
+  Mn: Multiply counter by n, n is a custom integer
+  Ln: Divide counter by n, n is a custom integer
   ''')
-  choice = input("Your choice of operation: ")
-  try:
-    o = choice.lower()[0]
-    n = int(choice.strip(" ")[1:])
-    if o not in ['i', 'd']:
-      raise Exception
-    return (o, n)
-  except Exception:
-    print("Invalid operation format!")
-    return False
+    choice = input("Your choice of operation: ")
+    try:
+        o = choice.lower()[0]
+        n = int(choice.strip(" ")[1:])
+        if o not in ['i', 'd', 'm', 'l']:
+            raise Exception
+        return (o, n)
+    except Exception:
+        print("Invalid operation format!")
+        return False
+
 
 def get_number_of_concurrent_operations(peers):
-  _n = input(f"Enter the number of concurrent operations you want to perform or <Enter> to quit: ")
-  if _n == "":
-    print("Shutdown...")
-    for p in peers:
-      p.stop()
-    sys.exit()
-  try:
-    _n = int(_n)
-    if _n < 1:
-      print("Must have at least 1 operation!")
-      return False
-    return _n
-  except ValueError:
-    print("Must be integer!")
-    return False
+    sleep(2)
+    _n = input(
+        f"Enter the number of concurrent operations you want to perform or <Enter> to quit: "
+    )
+    if _n == "":
+        print("Shutdown...")
+        for p in peers:
+            p.shutdown = True
+            p.stop()
+        sys.exit()
+    try:
+        _n = int(_n)
+        if _n < 1:
+            print("Must have at least 1 operation!")
+            return False
+        return _n
+    except ValueError:
+        print("Must be integer!")
+        return False
+
 
 def get_peer(n, peers):
-  _id = input(f"Enter the peer ID to be controlled [1 - {n}]: ")
-  try:
-    _id = int(_id)
-    if 1 <= _id <= n:
-      return peers[_id-1]
-    else:
-      print("Peer ID out of range!")
-      return False
-  except ValueError:
-    print("Must be integer!")
-    return False
+    _id = input(f"Enter the peer ID to be controlled [1 - {n}]: ")
+    try:
+        _id = int(_id)
+        if 1 <= _id <= n:
+            return peers[_id - 1]
+        else:
+            print("Peer ID out of range!")
+            return False
+    except ValueError:
+        print("Must be integer!")
+        return False
+
 
 def print_peers_and_counter(peers):
-  print("")
-  for idx, p in enumerate(peers):
-    print(f"Peer ID: {idx+1}\tCounter value: {p.counter}")
-  print("")
+    print("")
+    for idx, p in enumerate(peers):
+        print(f"Peer ID: {idx+1}\tCounter value: {p.counter}")
+    print("")
+
 
 def thread_send_msg(peer, delay, operations):
-  while len(operations) > 0:
-    op = operations.pop()
-    sleep(randint(1, delay)/1000) # simulate various network delay
-    operation = op[0]
-    val = op[1]
-    if operation == 'i':
-      peer.send_to_nodes(f"Update >>> {commands.INCREMENT}{val}{commands.INCREMENT}", exclude=peer.nodes_inbound)
-      peer.counter += val
-    elif operation == 'd':
-      peer.send_to_nodes(f"Update >>> {commands.DECREMENT}{val}{commands.DECREMENT}", exclude=peer.nodes_inbound)
-      peer.counter -= val
+    while len(operations) > 0:
+        op = operations.pop()
+        sleep(randint(1, delay) / 1000)  # simulate various network delay
+        operation = op[0]
+        val = op[1]
+        data = ""
+        if operation == 'i':
+            data = f"Update >>> {commands.INCREMENT}{val}{commands.INCREMENT}"
+        elif operation == 'd':
+            data = f"Update >>> {commands.DECREMENT}{val}{commands.DECREMENT}"
+        elif operation == 'm':
+            data = f"Update >>> {commands.MULTIPLY}{val}{commands.MULTIPLY}"
+        elif operation == 'l':
+            data = f"Update >>> {commands.DIVIDE}{val}{commands.DIVIDE}"
+        peer.operations = merge_operations(peer.operations, data)
+        peer.send_to_nodes(data, exclude=peer.nodes_inbound)
+
+
+def merge_operations(operations, data):
+    if commands.INCREMENT in data:
+        operations.append(data)
+    elif commands.DECREMENT in data:
+        operations.append(data)
+    elif commands.MULTIPLY in data:
+        operations.insert(0, data)
+    elif commands.DIVIDE in data:
+        operations.insert(0, data)
+    return operations
